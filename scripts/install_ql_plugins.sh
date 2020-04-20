b158e5dfddc7368234afb0de450671cdb3491b89
davdroman <d@vidroman.dev>
2020-04-20 03:29:04 +0100

    Remove `suspicious-package` app

--- scripts/install_ql_plugins.sh
+++ scripts/install_ql_plugins.sh
@@ -4,7 +4,6 @@ brew cask install qlcolorcode
 brew cask install qlstephen
 brew cask install qlmarkdown
 brew cask install quicklook-json
-brew cask install suspicious-package
 brew cask install quicklookase
 brew cask install qlvideo
 brew cask install provisionql
