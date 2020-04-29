6b8bc3bed75feceefb9a69f0f3ebd1a25d7519db
davdroman <d@vidroman.dev>
2020-04-29 01:40:22 +0100

    Add reset_launchpad function

--- sources/mackup/.config/fish/functions/macos.fish
+++ sources/mackup/.config/fish/functions/macos.fish
@@ -3,3 +3,8 @@ set MACOS_SETUP_PATH "$HOME/Library/Mobile\ Documents/com~apple~CloudDocs/macOS"
 alias cd_macos  "cd $MACOS_SETUP_PATH"
 alias tf_macos   "open -a Finder $MACOS_SETUP_PATH"
 alias subl_macos "subl $MACOS_SETUP_PATH"
+
+function reset_launchpad
+    defaults write com.apple.dock ResetLaunchPad -bool true
+    killall Dock
+end
