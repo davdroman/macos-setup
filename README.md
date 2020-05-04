7772e4149f163cec0ace4af329454ec4e7ba739e
davdroman <d@vidroman.dev>
2020-05-04 01:17:06 +0100

    Use Brewfiles

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-From a fresh install of macOS's Terminal:
+Fork this repo. From a fresh install of macOS's Terminal:
 
-1. `curl -sS https://github.com/davdroman/macos-setup/archive/master.zip > macOS.zip`
-1. `unzip macOS.zip`
-1. `cp macOS "$HOME/Library/Mobile Documents/com~apple~CloudDocs" && rm macOS.zip`
-1. `cd "$HOME/Library/Mobile Documents/com~apple~CloudDocs/macOS/scripts"`
-1. `./setup.sh`
+1. `git clone https://github.com/[username]/macos-setup ~/Library/Mobile\ Documents/com~apple~CloudDocs/macOS`
+1. `~/Library/Mobile\ Documents/com~apple~CloudDocs/macOS/scripts/setup.sh`
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,61 @@
 #!/usr/bin/env bash
 
-SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
+# Prompts
 
-"$SCRIPT_PATH/install_cli_tools.sh"
-"$SCRIPT_PATH/install_apps.sh"
-"$SCRIPT_PATH/install_ql_plugins.sh"
-"$SCRIPT_PATH/install_fonts.sh"
-"$SCRIPT_PATH/set_defaults.sh"
+ask() {
+    echo -n "$1 " >&2
+    read -e should_install
+    echo $should_install
+}
+
+should_install_plex_ms=`ask "Is this machine a Plex server? (y/n)"`
+
+# Xcode + CLI Tools
+
+if ! which xcversion >/dev/null; then
+    GEM_HOME=~/.gem gem install xcode-install
+fi
+
+function xcversion () {
+    GEM_HOME=~/.gem ~/.gem/bin/xcversion "$@"
+}
+
+xcversion list
+xcode_version=`ask "Which Xcode version to install?"`
+xcversion install "$xcode_version"
+
+# Homebrew
+
+if ! which brew >/dev/null; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+DIR=$(dirname "$(realpath -s "$0")")
+
+brew bundle --file "$DIR/Brewfile-Core" --no-lock
+
+if [ "$should_install_plex_ms" != "${should_install_plex_ms#[Yy]}" ]; then
+    brew bundle --file "$DIR/Brewfile-PlexServer" --no-lock
+    mkdir -p "$HOME/Downloads/Torrent/Complete"
+    mkdir -p "$HOME/Downloads/Torrent/Incomplete"
+else
+    brew bundle --file "$DIR/Brewfile-Default" --no-lock
+fi
+
+# Install fonts in resources folder
+
+cp -f $HOME/Library/Mobile\ Documents/com~apple~CloudDocs/macOS/resources/fonts/* $HOME/Library/Fonts
+
+# Allow QuickLook plugins
+
+xattr -d -r com.apple.quarantine ~/Library/QuickLook
+
+# Set defaults
+
+mkdir -p "$HOME/Development"
+
+ln -sf "$HOME/Library/Mobile Documents/com~apple~CloudDocs/macOS/sources/mackup/.mackup.cfg" "$HOME"
+ln -sf "$HOME/Library/Mobile Documents/com~apple~CloudDocs/macOS/sources/.mackup" "$HOME"
+mackup restore -f
+
+defaults write com.apple.iphonesimulator AllowFullscreenMode -bool YES
