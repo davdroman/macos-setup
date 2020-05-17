9ce271f4cca4ed1eac394debffb84520f6a716cc
davdroman <d@vidroman.dev>
2020-05-17 01:59:01 +0100

    Gardening

--- sources/mackup/.config/fish/functions/macos.fish
+++ sources/mackup/.config/fish/functions/macos.fish
@@ -16,3 +16,12 @@ function reset_launchpad
     defaults write com.apple.dock ResetLaunchPad -bool true
     killall Dock
 end
+
+function update_all_packages
+    gem update && \
+    gem cleanup && \
+    brew update && \
+    brew cask upgrade && \
+    brew upgrade && \
+    brew cleanup
+end
--- sources/mackup/.config/fish/functions/packages.fish
+++
@@ -1,9 +0,0 @@
-function update_all_packages
-    brew update && \
-    brew cask upgrade && \
-    gem update && \
-    gem cleanup && \
-    npm update -g && \
-    brew upgrade && \
-    brew cleanup
-end
