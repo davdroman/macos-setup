d396995c58e7d9110371992f76a7191c48a0834b
davdroman <d@vidroman.dev>
2020-07-02 13:42:30 +0100

    Implement video compression commands

--- sources/mackup/.config/fish/functions/tools.fish
+++ sources/mackup/.config/fish/functions/tools.fish
@@ -11,3 +11,27 @@ function ytdl
     set url (pbpaste)
     youtube-dl "$url" --ignore-errors --continue --format mp4
 end
+
+function video_compress
+    set input_path $argv
+
+    set dir (dirname $input_path)
+    set file (basename $input_path)
+    set file_name (echo "$file" | cut -d '.' -f1)
+
+    set output_path $dir/$file_name"_compressed.mp4"
+
+    ffmpeg -v quiet -stats -i "$input_path" -vcodec h264 -acodec aac "$output_path"
+end
+
+function video_compress_max
+    set input_path $argv
+
+    set dir (dirname $input_path)
+    set file (basename $input_path)
+    set file_name (echo "$file" | cut -d '.' -f1)
+
+    set output_path $dir/$file_name"_compressed_max.mp4"
+
+    ffmpeg -v quiet -stats -i "$input_path" -vcodec libx265 -crf 28 "$output_path"
+end
